bfdbae51392d0d36d046ea15a106a35e10846203
nirgluzman <nir.gluzman@gmail.com>
2023-11-19 00:42:16 +0100

    basic Node.js TypeScript setup

---
+++ README.txt
@@ -0,0 +1,7 @@
+Source: Coding a Simple WebSocket Server in Node.js that Scales!
+https://www.youtube.com/watch?v=Gq7fenbjehs
+https://github.com/covalence-io/ws-simple
+https://github.com/covalence-io/express-template
+
+
+
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response, Application } from 'express';
+
+const app: Application = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to Express & TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`Listen to http://localhost:${port}`);
+});

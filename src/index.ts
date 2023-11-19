d08644091d20965c6b5669a81a43191d60fca84d
nirgluzman <nir.gluzman@gmail.com>
2023-11-19 11:29:00 +0100

    add colors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express, { Request, Response, Application } from 'express';
+import 'colors';
 
 const app: Application = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server');
 });
 
 app.listen(port, () => {
-  console.log(`Listen to http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`.red);
 });
